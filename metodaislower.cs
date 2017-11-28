cbc263b2b0d18692a2aea2188b13da0b1a626d79
lukinysa <33897947+lukinysa@users.noreply.github.com>
2017-11-28 17:41:40 +0100

    Add files via upload

---
+++ metodaislower.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Wpisz jakiekolwiek 2znaki: ");
+            string wyraz = Console.ReadLine();
+            if (wyraz.Length > 2)
+            {
+                Console.WriteLine("Miałeś wpisać 2znaki");
+                Console.ReadKey();
+                Main();
+            }
+            int i;
+            for (i = 0; i < wyraz.Length; i++)
+            {
+                Console.Write(wyraz[i] + " ");
+                if (Char.IsLower(wyraz[i]))
+                {
+                    Console.WriteLine("jest mała");
+                }
+                if (Char.IsUpper(wyraz[i]))
+                {
+                    Console.WriteLine("jest wielka");
+
+                }
+                else
+                {
+                    Console.WriteLine("Nie jest literą");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
